682e3f212f9babfcab7c2d1da77113401eabcb21
Shawn Azar <cydiasa@gmail.com>
2023-11-30 22:23:50 -0700

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { type Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react';
 
 import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
@@ -32,6 +33,7 @@ export default function RootLayout({
           <div className="flex w-full">
             <Layout>{children}</Layout>
           </div>
+          <Analytics />
         </Providers>
       </body>
     </html>
