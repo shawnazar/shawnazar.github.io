2cb179843ae3234cb0d6cbd4e47dce5db6821609
Shawn Azar <cydiasa@gmail.com>
2023-12-07 16:58:13 -0700

    updated name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,12 +8,12 @@ import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Spencer Sharp',
+    template: '%s - Shawn Azar',
     default:
-      'Spencer Sharp - Software designer, founder, and amateur astronaut',
+      'Shawn Azar - Software designer, founder, and amateur astronaut',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'I’m Spencer, a software designer and entrepreneur based in Denver. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
