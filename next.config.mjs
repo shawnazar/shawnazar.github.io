10a46c87c7a904f9105324915aa0ed4ac825c5aa
Shawn Azar <cydiasa@gmail.com>
2023-12-08 17:06:17 -0700

    removed export and home lab section

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ import remarkGfm from 'remark-gfm'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  output: "export"
 }
 
 const withMDX = nextMDX({
